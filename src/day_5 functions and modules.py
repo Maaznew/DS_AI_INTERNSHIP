5ceefcfbd7007b63814cdf0f6d0da5b652db685b
mohammed maaz <maazck4@gmail.com>
2026-02-06 16:26:39 +0530

    Day5:function and modules

---
+++ src/day_5 functions and modules.py
@@ -0,0 +1,31 @@
+def greed():
+    print("iam mohammed maaz")
+    
+greed()
+
+
+def greed():
+    print("welcome to day5 of internship")
+    
+greed()
+
+def add(a,b):
+    print("a+b")
+    result=add(3,5)
+    print(result
+
+#task 1
+
+def calc_rectangle(length, width):
+    area = length * width
+    perimeter = 2 * (length + width)
+    return area, perimeter
+
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+area, perimeter = calc_rectangle(length, width)
+print(f"Area: {area}, Perimeter: {perimeter}")
+ 
+
+    
---
+++ src/math_operations.py
@@ -0,0 +1,10 @@
+def power(base, exp):
+    return base ** exp
+
+#calculating average
+def average(list_numbers):
+    if len(list_numbers)==0:
+        return 0
+    return sum(list_numbers)/len(list_numbers)
+
+
